3fc49a429128819592439f3909319ca3aad1b8e9
Sina Jafari <168327662+sina0101@users.noreply.github.com>
2024-09-04 19:34:51 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# install git 
-sudo apt install git -y
-
-# clone from repo in xui-assistant directory
-git clone https://github.com/sina0101/asiss-xui.git/root/assis-xui/
-
 
 
 # مسیر فعلی را به دست می‌آورد
